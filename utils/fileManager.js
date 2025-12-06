b7325f0046db420696d1867ba52b99243748efae
kdfuzzy <kyedow0412@gmail.com>
2025-12-06 01:22:14 +0000

    Add fileManager module for JSON loading and saving

--- utils/fileManager.js
+++ utils/fileManager.js
@@ -1,1 +1,22 @@
+const fs = require("fs");
 
+module.exports = {
+    loadJSON(path) {
+        try {
+            if (!fs.existsSync(path)) return {};
+            const data = fs.readFileSync(path, "utf8");
+            return JSON.parse(data || "{}");
+        } catch (err) {
+            console.error("JSON LOAD ERROR:", err);
+            return {};
+        }
+    },
+
+    saveJSON(path, data) {
+        try {
+            fs.writeFileSync(path, JSON.stringify(data, null, 4));
+        } catch (err) {
+            console.error("JSON SAVE ERROR:", err);
+        }
+    }
+};
