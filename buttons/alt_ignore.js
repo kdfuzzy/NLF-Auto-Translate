6a5ce586d7f34e32022defc29fb531bf1ea98be7
kdfuzzy <kyedow0412@gmail.com>
2025-12-07 22:22:02 +0000

    Update alt_ignore.js

--- buttons/alt_ignore.js
+++ buttons/alt_ignore.js
@@ -2,8 +2,12 @@ module.exports = {
     id: /^alt_ignore_/,
 
     async execute(interaction) {
-        interaction.reply({
-            content: "🟢 Alert ignored.",
+        return interaction.reply({
+            embeds: [{
+                title: "🟢 Alert Ignored",
+                description: "This alt alert has been dismissed.",
+                color: 0x00ff66
+            }],
             ephemeral: true
         });
     }
