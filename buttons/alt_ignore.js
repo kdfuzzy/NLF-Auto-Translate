d16bbde28ad6217cea7e85f9f6e397c97a2478ca
kdfuzzy <kyedow0412@gmail.com>
2025-12-08 00:23:13 +0000

    Update alt_ignore.js

--- buttons/alt_ignore.js
+++ buttons/alt_ignore.js
@@ -1,14 +1,29 @@
+const fs = require("fs");
+const { EmbedBuilder } = require("discord.js");
+
 module.exports = {
     id: /^alt_ignore_/,
 
-    async execute(interaction) {
+    async execute(interaction, client, config) {
+        const userId = interaction.customId.split("_")[2];
+
+        // Load DB
+        const path = "./stats/altdb.json";
+        const altdb = JSON.parse(fs.readFileSync(path));
+
+        // Mark ignored
+        if (!altdb.users[userId]) altdb.users[userId] = {};
+        altdb.users[userId].ignored = true;
+
+        fs.writeFileSync(path, JSON.stringify(altdb, null, 2));
+
         return interaction.reply({
-            embeds: [{
-                title: "🟢 Alert Ignored",
-                description: "This alt alert has been dismissed.",
-                color: 0x00ff66
-            }],
-            ephemeral: true
+            embeds: [
+                new EmbedBuilder()
+                    .setColor("Grey")
+                    .setTitle("👁️ User Ignored")
+                    .setDescription(`User <@${userId}> has been marked as **ignored**.`)
+            ]
         });
     }
 };
